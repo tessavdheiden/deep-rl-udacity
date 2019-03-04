45bb78d975eab106bded685ce9d22ea391e6f0d0
Tessa Heiden <tessavdheiden@gmail.com>
2019-03-04 16:18:41 +0100

    added double dqn

--- training/q_learning.py
+++ training/q_learning.py
@@ -37,3 +37,36 @@ def dqn(agent, env, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, ep
             torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
             break
     return scores
+
+def dqn_unity(agent, env, brain_name, env_info, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):  
+    
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=True)[brain_name] # reset the environment
+        state = env_info.vector_observations[0]            # get the current state
+        score = 0                                          # initialize the score 
+        for t in range(max_t):
+            action = agent.act(state, eps)                # select an action
+            env_info = env.step(action)[brain_name]        # send the action to the environment
+            next_state = env_info.vector_observations[0]   # get the next state
+            reward = env_info.rewards[0]                   # get the reward
+            done = env_info.local_done[0]                  # see if episode has finished
+            agent.step(state, action, reward, next_state, done)
+            score += reward                                # update the score
+            state = next_state                             # roll over the state to next time step
+            if done:                                       # exit loop if episode finished
+                break
+
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>=200.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+            break
+    return scores
