f4e1ff808e0861d141c72fc785a99f4c939f0723
Tessa Heiden <tessavdheiden@gmail.com>
2019-03-20 10:51:31 +0100

    soft w

--- p2/models/modelRNN.py
+++ p2/models/modelRNN.py
@@ -3,83 +3,86 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+FLOAT = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
 
 def hidden_init(layer):
     fan_in = layer.weight.data.size()[0]
     lim = 1. / np.sqrt(fan_in)
     return (-lim, lim)
 
+def fanin_init(size, fanin=None):
+    fanin = fanin or size[0]
+    v = 1. / np.sqrt(fanin)
+    return torch.Tensor(size).uniform_(-v, v)
+
 class ActorRNN(nn.Module):
-    """Actor (Policy) Model."""
-
-    def __init__(self, state_size, action_size, seed, fc_units=256):
-        """Initialize parameters and build model.
-        Params
-        ======
-            state_size (int): Dimension of each state
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-            fc1_units (int): Number of nodes in first hidden layer
-            fc2_units (int): Number of nodes in second hidden layer
-        """
+    def __init__(self, nb_states, nb_actions, init_w=3e-3):
         super(ActorRNN, self).__init__()
-        self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, fc_units)
-        self.lstm = nn.LSTM(fc_units, fc_units, 1, dropout=0.0)
-        self.fc2 = nn.Linear(fc_units, action_size)
-        self.reset_parameters()
+        self.fc1 = nn.Linear(nb_states, 20)
+        self.fc2 = nn.Linear(20, 50)
+        self.lstm = nn.LSTMCell(50, 50)
+        self.fc3 = nn.Linear(50, nb_actions)
+        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
+        self.init_weights(init_w)
 
-    def reset_parameters(self):
-        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(-3e-3, 3e-3)
+        self.cx = Variable(torch.zeros(1, 50)).type(FLOAT)
+        self.hx = Variable(torch.zeros(1, 50)).type(FLOAT)
 
-    def forward(self, states):
-        """Build an actor (policy) network that maps states -> actions."""
-        x = self.lstm(states)
-        x = F.relu(self.fc1(x))
-        return F.tanh(self.fc2(x))
+    def init_weights(self, init_w):
+        self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+        self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+        self.fc3.weight.data.uniform_(-init_w, init_w)
+
+    def reset_lstm_hidden_state(self, done=True):
+        if done == True:
+            self.cx = Variable(torch.zeros(1, 50)).type(FLOAT)
+            self.hx = Variable(torch.zeros(1, 50)).type(FLOAT)
+        else:
+            self.cx = Variable(self.cx.data).type(FLOAT)
+            self.hx = Variable(self.hx.data).type(FLOAT)
+
+    def forward(self, x, hidden_states=None):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+
+        if hidden_states == None:
+            hx, cx = self.lstm(x, (self.hx, self.cx))
+            self.hx = hx
+            self.cx = cx
+        else:
+            hx, cx = self.lstm(x, hidden_states)
+
+        x = hx
+        x = self.fc3(x)
+        x = self.tanh(x)
+        return x, (hx, cx)
 
 
 class CriticRNN(nn.Module):
-    """Critic (Value) Model."""
-
-    def __init__(self, state_size, action_size, seed, fcs1_units=256, fc2_units=256, fc3_units=128):
-        """Initialize parameters and build model.
-        Params
-        ======
-            state_size (int): Dimension of each state
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-            fcs1_units (int): Number of nodes in the first hidden layer
-            fc2_units (int): Number of nodes in the second hidden layer
-        """
+    def __init__(self, nb_states, nb_actions, init_w=3e-3):
         super(CriticRNN, self).__init__()
-        hidden_dim = 64
-        self.seed = torch.manual_seed(seed)
-        self.encode_state = nn.LSTM(state_size, hidden_dim, 1, dropout=0.0)
-        self.fc1 = nn.Linear(hidden_dim, fcs1_units)
+        self.fc1 = nn.Linear(nb_states, 20)
+        self.fc2 = nn.Linear(20 + nb_actions, 50)
+        self.fc3 = nn.Linear(50, 1)
+        self.relu = nn.ReLU()
+        self.init_weights(init_w)
 
-        self.encode_action = nn.LSTM(action_size, hidden_dim, 1, dropout=0.0)
-        self.fc2 = nn.Linear(hidden_dim, fc2_units)
+    def init_weights(self, init_w):
+        self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+        self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+        self.fc3.weight.data.uniform_(-init_w, init_w)
 
-        self.fc3 = nn.Linear(fcs1_units + fc2_units, fc3_units)
-        self.fc4 = nn.Linear(fc3_units, 1)
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        self.encode_state.weight.data.uniform_(*hidden_init(self.encode_state))
-        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
-        self.encode_action.weight.data.uniform_(*hidden_init(self.encode_action))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
-        self.fc4.weight.data.uniform_(-3e-3, 3e-3)
-
-    def forward(self, states, actions):
-        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        state = self.lstm(states)
-        action = self.lstm(actions)
-        xs = F.leaky_relu(self.fcs1(state))
-        xa = F.leaky_relu(self.fcs2(action))
-        x = torch.cat((xs, xa), dim=1)
-        x = F.leaky_relu(self.fc3(x))
-        return self.fc4(x)
+    def forward(self, xs):
+        x, a = xs
+        out = self.fc1(x)
+        out = self.relu(out)
+        # debug()
+        # out = self.fc2(torch.cat([out,a],dim=1)) # dim should be 1, why doesn't work?
+        out = self.fc2(torch.cat([out, a], 1))  # dim should be 1, why doesn't work?
+        out = self.relu(out)
+        out = self.fc3(out)
+        return out
